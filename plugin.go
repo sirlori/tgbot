b19cb2152d4b280b6b5a241f5b4a8ea28f7949ca
Lorenzo Landolfi <lorenzo.landolfi97@gmail.com>
2015-07-02 18:14:03 +0200

    Plugin interface becomes struct

--- plugin.go
+++ plugin.go
@@ -1,18 +1,44 @@
 package tgbot
 
-// Plugin is the interface for all basic methods a plugin must have.
-type Plugin interface {
-	// MUST BE UNIQUE name of plugin
-	GetName() string
-	// Slice of available commands, WITH forward slash (if needed)
-	GetCommands() MultipleParser
-	// Slice of help text messages, sent below the plugin name in help command
-	GetHelpText() []string
-	// Function to set up needed configuration, etc.
-	Setup(*Bot) bool
-	// Called when Message is received, params are Message, command, args
+import (
+	"errors"
+	"log"
+)
+
+type Plugin struct {
+	Name     string
+	Commands MultipleParser
+	setups   []func(*MultipleParser) error
 }
 
+//Setup your plugin
+func (plugin *Plugin) Setup(debug bool) error {
+	var err error
+	for _, f := range plugin.setups {
+		err = f(&plugin.Commands)
+		if debug {
+			log.Printf(err.Error())
+		}
+	}
+	if err != nil {
+		return errors.New("The commands setups returned errors")
+	}
+	return nil
+}
+
+//Add setup that let's you create a command for your plugin
+func (plugin *Plugin) AddSetup(setup func(*MultipleParser) error) {
+	plugin.setups = append(plugin.setups, setup)
+}
+
+//Add a plugin to your bot
 func (bot *Bot) AddPlugin(plugin Plugin) {
 	bot.Plugins = append(bot.Plugins, plugin)
+}
+
+func NewPlugin(name string) Plugin {
+	plugin := Plugin{}
+	plugin.Name = name
+	plugin.Commands = NewMultipleParser()
+	return plugin
 }
